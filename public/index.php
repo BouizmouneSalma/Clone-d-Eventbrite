3e3f30059567b8f38c5c0511946ba5f22a702e78
Safaa-Ettalhi <safaeettalhi@gmail.com>
2025-02-15 21:20:09 +0100

    add route

--- public/index.php
+++ public/index.php
@@ -13,6 +13,7 @@ $router = new Router();
 
 // User routes
 $router->addRoute('GET', '/', 'EventController', 'index');
+$router->addRoute('GET', '/dashboard', 'UserController', 'dashboard');
 $router->addRoute('GET', '/login', 'UserController', 'login');
 $router->addRoute('POST', '/login', 'UserController', 'login');
 $router->addRoute('GET', '/register', 'UserController', 'register');
@@ -26,6 +27,8 @@ $router->addRoute('POST', '/events/create', 'EventController', 'create');
 $router->addRoute('GET', '/events/{id}', 'EventController', 'detail');
 $router->addRoute('GET', '/events/del/{id}', 'EventController', 'delete');
 //$router->addRoute('GET', '/ticket/pdf/{id}', 'TicketController', 'generateTicketPDF');
+
+
 
 
 // Ticket routes
@@ -42,12 +45,19 @@ $router->addRoute('POST', '/admin/users/update', 'AdminController', 'updateUser'
 $router->addRoute('POST', '/admin/users/delete', 'AdminController', 'deleteUser');
 $router->addRoute('POST', '/admin/users/ban', 'AdminController', 'banUser');
 $router->addRoute('POST', '/admin/users/unban', 'AdminController', 'unbanUser');
-
+$router->addRoute('GET', '/reservations/cancel/{id}', 'UserController', 'cancelReservation');
 // Organizer routes
 $router->addRoute('GET', '/organizer/dashboard', 'OrganizerController', 'dashboard');
 $router->addRoute('GET', '/organizer/promo-codes/create', 'OrganizerController', 'createPromoCode');
 $router->addRoute('POST', '/organizer/promo-codes/create', 'OrganizerController', 'createPromoCode');
-//$router->addRoute('GET', '/organizer/export-participants/{id}', 'OrganizerController', 'exportParticipants');
+$router->addRoute('GET', '/organizer/promo-codes/edit/{id}', 'OrganizerController', 'editPromoCode');
+$router->addRoute('POST', '/organizer/promo-codes/edit/{id}', 'OrganizerController', 'editPromoCode');
+$router->addRoute('GET', '/organizer/promo-codes/delete/{id}', 'OrganizerController', 'deletePromoCode');
+$router->addRoute('GET', '/organizer/manage-sales', 'OrganizerController', 'manageSales');
+$router->addRoute('GET', '/organizer/cancel-reservation/{id}', 'OrganizerController', 'cancelReservation');
+$router->addRoute('GET', '/organizer/active-reservation/{id}', 'OrganizerController', 'ActiveReservation');
+$router->addRoute('GET', '/organizer/promo-codes', 'OrganizerController', 'listPromoCodes');
+
 
 $method = $_SERVER['REQUEST_METHOD'];
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
